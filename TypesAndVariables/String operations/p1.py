5b9a61ef4dfbb0060f5628a3daeeacedc175064b
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2024-11-15 17:05:25 +0100

    Update p1.py

--- TypesAndVariables/String operations/p1.py
+++ TypesAndVariables/String operations/p1.py
@@ -2,7 +2,7 @@
 # A program that calculates the number of characters
 # of your name, surname and full name
 #
-name = 'Domink'   # replace Anna with your name
+name = 'Dominik'   # replace Anna with your name
 surname = 'Kawula' # replace May with your surname
 characters_in_name = len(name)
 characters_in_surname = len(surname)
@@ -12,6 +12,6 @@ print(f'Your surname has {characters_in_surname} characters')
 print(f'Your full name has {fullname} characters') # with a space between name and surname
 
 #output:
-#Your name has 6 characters
+#Your name has 7 characters
 #Your surname has 6 characters
-#Your full name has 13 characters
+#Your full name has 14 characters
