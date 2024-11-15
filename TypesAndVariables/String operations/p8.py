0589f1d74b0d3947a04a2a5068456f1ca0a26bc3
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2024-11-15 17:07:02 +0100

    Update p8.py

--- TypesAndVariables/String operations/p8.py
+++ TypesAndVariables/String operations/p8.py
@@ -9,3 +9,18 @@ last_letter_code = ord(last)
 
 number_of_letters = max(abs(last_letter_code - first_letter_code)-1,0)
 print(f'Between {first} and {last} is {number_of_letters} letters')
+
+
+
+#output for A and D
+"""
+Enter first letter: A
+Enter second letter: D
+Between A and D is 2 letters
+"""
+#output for K and K
+"""
+Enter first letter: K
+Enter second letter: K
+Between K and K is 0 letters
+"""
