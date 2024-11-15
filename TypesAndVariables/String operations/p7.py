3c712403aa280637de90b4ea20979cbb1ffb3b64
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2024-11-15 17:04:36 +0100

    Update p7.py

--- TypesAndVariables/String operations/p7.py
+++ TypesAndVariables/String operations/p7.py
@@ -12,3 +12,14 @@ print(f'The letter {name[5]} has a code {ord(name[5])}')
 print(f'The letter {name[6]} has a code {ord(name[6])}')
 ...
 ...
+#output:
+"""
+7
+The letter D has a code 68
+The letter o has a code 111
+The letter m has a code 109
+The letter i has a code 105
+The letter n has a code 110
+The letter i has a code 105
+The letter k has a code 107
+"""
