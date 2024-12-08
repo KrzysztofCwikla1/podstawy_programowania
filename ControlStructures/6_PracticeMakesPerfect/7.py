b1a165514aacd48bc19f88c56efa91db36fe0a51
Suselinho <121863629+Suselinho@users.noreply.github.com>
2024-12-08 16:42:18 +0100

    Add files via upload

---
+++ ControlStructures/6_PracticeMakesPerfect/7.py
@@ -0,0 +1,10 @@
+age= int(input('What is your age? '))
+
+if age < 13:
+ print('You are a child.')
+elif age >= 13 and age <= 19 :
+ print('You are a teen.')
+elif age >= 20 and age <= 64 :
+  print('You are an adult.')
+elif age >= 65:
+  print('You are a senior.')
